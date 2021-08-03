a079174ac91a2e04c18763ff8e61edb5436b12f9
Michelleeby <38360136+Michelleeby@users.noreply.github.com>
2021-08-03 06:51:36 -0400

    Update index.php

--- solr-search/solr-search/results/index.php
+++ solr-search/solr-search/results/index.php
@@ -5,6 +5,8 @@
  * @subpackage  solr-search
  * @copyright   2012 Rector and Board of Visitors, University of Virginia
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ * @author      Scholar's Lab
+ * @author      Michelle Byrnes
  */
 
 ?>
